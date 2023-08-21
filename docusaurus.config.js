b1831364c39b61780752402e43969a6c45c17aae
ronaldo <kun.xie@qiuweigroup.com>
2023-08-21 15:18:25 +0800

    修改站点

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://www.aishort.top",
+  url: "https://prompt.nextaibots.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
